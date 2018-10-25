2cfc6e96e26302628d2eb9783716b5a9c5e7c529
Omar Betancourt <omar.betancourt@gmail.com>
2018-10-24 22:29:55 -0700

    Third Commit

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Brody Johnson");
 		System.out.println("219216908 - This line was added by Jason Starace");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("214497245 - This line was added by Omar Betancourt");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
