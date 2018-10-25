51f577ffa6d3491b68c29bddda97b6bb7c811129
jasonstarace <jasonstarace@10.0.1.98>
2018-10-24 22:06:05 -0700

    Hmmmm

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Brody Johnson");
-		System.out.println("219216908 - This line was added by Jason Starace.");
+		System.out.println("219216908 - This line was added by Jason Starace");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
