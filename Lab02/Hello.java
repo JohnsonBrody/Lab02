14b16993308c9b8ded3d67425a5bba641dffd00a
Omar Betancourt <omarbetancourt@csus.edu>
2018-10-25 23:04:20 -0700

    Fifth Commit

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -5,11 +5,11 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("219265619 - This line was added by Brody Johnson.");
-		System.out.println("219216908 - This line was added by Jason Starace.");
-		System.out.println("214497245 - This line was added by Omar Betancourt.");
-		System.out.println("213535915 - This line was added by Faisl Qurishi.");
-		System.out.println("219709764 - This line was added by Anshul Shandilya.");
+		System.out.println("219265619 - This line was added by Brody Johnson");
+		System.out.println("219216908 - This line was added by Jason Starace");
+		System.out.println("214497245 - This line was added by Omar Betancourt");
+		System.out.println("213535915 - This line was added by Faisl Qurishi");
+		System.out.println("219709764 - This line was added by Anshul Shandilya");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
