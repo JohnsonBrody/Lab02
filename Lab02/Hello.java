21fcc54946231bd9b1b25094029e167e7c1a5c39
Brody <Brody@10.0.0.80>
2018-10-25 21:42:57 -0700

    Added my ID (Brody)

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Brody Johnson");
+		System.out.println("219265619 - This line was added by Brody Johnson");
 		System.out.println("219216908 - This line was added by Jason Starace");
 		System.out.println("214497245 - This line was added by Omar Betancourt");
 		System.out.println("213535915 - This line was added by Faisl Qurishi");
