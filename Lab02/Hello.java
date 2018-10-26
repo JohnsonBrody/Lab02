b0b20c7a79ea952416eece7b53fb79704a75a38e
akaid <akaid@10.0.0.41>
2018-10-25 21:50:52 -0700

    Added name and id

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("219216908 - This line was added by Jason Starace");
 		System.out.println("214497245 - This line was added by Omar Betancourt");
 		System.out.println("213535915 - This line was added by Faisl Qurishi");
-		System.out.println("219709764 - This line was added by Anshul Shandilya.");
+		System.out.println("219709764 - This line was added by Anshul Shandilya");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
